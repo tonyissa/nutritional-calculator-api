b4666e8e0ad9adffdae2f7c102aacc0ac3f29665
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-11-26 18:30:36 -0500

    Configure project to use xml documentation for Swagger

--- nutritional-calculator-api/Program.cs
+++ nutritional-calculator-api/Program.cs
@@ -1,17 +1,34 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using nutritional_calculator_api.Data;
 using nutritional_calculator_api.Helpers;
 using nutritional_calculator_api.Options;
+using System.Reflection;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("SqliteConnection")
     ?? throw new InvalidOperationException("Sqlite database connection string not found");
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers().AddJsonOptions(x =>
+   x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "V1",
+        Title = "Nutrition Calculator API",
+        Description = "API for retrieving nutritional information for food"
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    s.IncludeXmlComments(xmlPath);
+});
+
 
 builder.Services.AddDbContext<NutritionContext>(opt =>
 {
@@ -30,7 +47,6 @@ using (var scope = app.Services.CreateScope())
     await SeedData.ExecuteAsync(context, dataOptions);
 }
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
